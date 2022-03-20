2027e5761273156208ad970a3ebff903034c943d
amorokhmanfendi <fendi.amorokhman@tokopedia.com>
2022-03-20 18:23:33 +0800

    init tailwind nextjs

---
+++ kelasin-aja/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-red-500">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
