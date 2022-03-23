3c1a59ab4558ab7b33c434a9af53dac9ecc5716d
Fendi Irfan Amorokhman <amorokhman@gmail.com>
2022-03-23 15:01:14 +0800

    Page dashboard pengajar dan buat conference

---
+++ kelasin-aja/pages/buat_conference.jsx
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+
+import Navbar from '../components/header/Navbar'
+import Sidebar from '../components/sidebar/Sidebar'
+import Footer from '../components/footer/Footer'
+
+export default function Home() {
+  return (
+    <div className="bg-slate-200">
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar />
+      <div className='flex'>
+        <Sidebar />
+        <div className="flex flex-col mx-auto mt-[25px] border-2">
+          <div className='  h-[245px] bg-[#C4C4C4] rounded-[10px] shadow'>
+            <p className='text-[64px] text-white font-sans text-center mt-20'>KELAS A</p>
+          </div>
+          <div className='bg-white rounded-[10px] w-[877px] h-[582px] mt-[22px] shadow-lg'>
+            <div className='ml-10 mr-10 mt-20 flex  justify-center '>
+              <div className='flex items-center justify-center  mr-[44px] w-[242px] h-[401px] bg-[#F2F2F2] rounded-10 shadow-lg'>
+                <button className='w-[179px] h-[39px] bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow'>
+                  Buat Link Meet
+                </button>
+              </div>
+              <div className='w-[488px] h-[401px]  bg-[#F2F2F2] rounded-10 shadow-lg'>
+                <div className='border-1 ml-[20px] mr-[20px] mt-[35px] flex flex-col justify-center items-center'>
+                  <p className='text-center font-bold text-[24px]'>Input Link Video Conference</p>
+
+                  <div className='mt-[20px]'>
+                    <input name="link"
+                      className="w-[447px] text-base px-4 py-2 border border-gray-600 rounded-[15px] focus:outline-none focus:border-black"
+                    />
+                  </div>
+                  <button class="bg-[#48D118] w-[179px] justify-center hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-[45px]">
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
---
+++ kelasin-aja/pages/dashboard_pengajar.jsx
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+
+import Navbar from '../components/header/Navbar'
+import Sidebar from '../components/sidebar/Sidebar'
+import Footer from '../components/footer/Footer'
+
+export default function Home() {
+  return (
+    <div className="bg-slate-200">
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar />
+      <div className='flex'>
+        <Sidebar />
+        <div className="flex flex-col w-[877px] mx-auto mt-[25px] border-2">
+          <div className=' w-[877px] h-[245px] bg-[#C4C4C4] rounded-[10px] shadow'>
+            <p className='text-[64px] text-white font-sans text-center mt-20'>KELAS A</p>
+          </div>
+          <div className='bg-white rounded-[10px] w-[877px] h-[231px] mt-[22px] shadow-lg'>
+            <div >
+              <div className='ml-10 mt-5'>
+                <p className='text-[36px] font-bold'>Membuat Post</p>
+              </div>
+              <div className='ml-10 mt-20 flex justify-center'>
+                <div className='grid grid-cols-3'>
+                  <button className='w-[252px] mr-[27px] bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow-lg'>
+                    Buat Video Conference
+                  </button>
+                  <button className='w-[252px] bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow'>
+                    Buat Tugas
+                  </button>
+                  <button className='w-[252px] bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow'>
+                    Upload Materi
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='bg-white rounded-[10px] w-[877px] h-[305px] mt-[22px] shadow-lg'>
+            <div className='ml-10 mr-10 mt-5 border-b-2 border-black'>
+              <p className='text-[50px] font-bold mb-10'>Tugas 1</p>
+            </div>
+            <div className='ml-10 mt-5'>
+              <p className='text-[32px]'>Kumpulkan tugas 1 kalian!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
