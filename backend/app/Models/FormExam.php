88b82ed4fa5d7888d6a61f4a6b6fa77007b03591
zendybramantia <zendyoke@gmail.com>
2022-04-06 15:09:17 +0700

    form_exam edit

--- backend/app/Models/FormExam.php
+++ backend/app/Models/FormExam.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class FormExam extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'class_id',
+        'title',
+        'description',
+        'link_embed_form'
+    ];
+
+    public function kelas() {
+        return $this->belongsTo(Kelas::class);
+    }
 }
