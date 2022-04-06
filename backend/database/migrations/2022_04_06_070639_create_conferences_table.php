7b5ce535f05b9e9c3bddd33e845c395e56f70a12
Fendi Irfan Amorokhman <amorokhman@gmail.com>
2022-04-06 15:33:14 +0800

    migrate

--- backend/database/migrations/2022_04_06_070639_create_conferences_table.php
+++ backend/database/migrations/2022_04_06_070639_create_conferences_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('conferences', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->timestamps();
+            $table->unsignedBigInteger('class_id');
+            $table->string('link_code_conference');
+
+            // Foreign key stuff
+            $table->foreign('class_id')->references('id')->on('kelas');
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('conferences');
     }
 };
--- backend/database/migrations/2022_04_06_070805_create_users_table.php
+++ backend/database/migrations/2022_04_06_070805_create_users_table.php
@@ -13,14 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
+            $table->unsignedBigInteger('class_id');
+            $table->string('jenis_user');
+            $table->string('nama');
+            $table->string('password');
+            $table->string('email')->unique();
+
+            // Foreign key stuff
+            $table->foreign('class_id')->references('id')->on('kelas');
+
         });
     }
 
@@ -31,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 };
