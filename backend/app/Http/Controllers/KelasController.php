9d38d98474beba4627ecb79c33ad298cd74f9f2c
Fendi Irfan Amorokhman <amorokhman@gmail.com>
2022-04-09 17:43:14 +0800

    Kelas

--- backend/app/Http/Controllers/KelasController.php
+++ backend/app/Http/Controllers/KelasController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Kelas;
 
 class KelasController extends Controller
 {
     //
+    public function index()
+    {
+        try{
+            $kelas = Kelas::with('user')->get();
+        }catch(\Exception $e){
+
+        }
+    }
+
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Kelas
+Route::get("/kelas",[\App\Http\Controllers\Api\KelasController::class,"index"]);
